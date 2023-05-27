7e3ff7313b894228341330ab5e3f04e3a78e396a
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-05-27 12:49:29 +0600

    googlelogin redirect page

--- src/Home/Alltoy.jsx
+++ src/Home/Alltoy.jsx
@@ -5,38 +5,12 @@ import { useEffect, useState } from "react";
  
 
 const Alltoy = () => {
-        const alltoys = useLoaderData();
-        const [searchText, setSearchText] = useState("");
-        const [toys, setToys] = useState('')
-
-          // useEffect(() => {
-          //   fetch('http://localhost:5000/alltoys')
-          //   .then(res => res.json())
-          //   .then(data =>  console.log(data))
-          // },[])
-
-         
-        const handleSearch = () => {
-          fetch(
-            `http://localhost:5000/alltoys/${searchText}`
-          )
-            .then((res) => res.json())
-            .then((data) => {
-              setToys(data);
-            });
-        };
-
+        const alltoys = useLoaderData(); 
         return (
                 <div>
                         <h1 className="text-2xl font-bold text-center my-6">All Toys: {alltoys.length}</h1>
 
-                        <input
-          onChange={(e) => setSearchText(e.target.value)}
-          onKeyUp={handleSearch}
-          type="text"
-          placeholder="Search here"
-          className="input input-bordered input-primary w-full max-w-xs text-center"
-        />
+                         
 
                         <div className="overflow-x-auto">
   <table className="table w-full">
