40a4dca77f8979fb49803a02604762d7195794a2
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-12-21 15:28:34 +0600

    update

--- src/Home/Alltoy.jsx
+++ src/Home/Alltoy.jsx
@@ -1,46 +1,37 @@
 import { useLoaderData } from "react-router-dom";
 import Toytable from "./Toytable";
-import { useEffect, useState } from "react";
-
  
 
 const Alltoy = () => {
-        const alltoys = useLoaderData(); 
-        return (
-                <div>
-                        <h1 className="text-2xl mt-28 font-bold text-center my-6 text-orange-300">All Toys: {alltoys.length}</h1>
+  const alltoys = useLoaderData();
+  return (
+    <div>
+      <h1 className="text-2xl mt-28 font-bold text-center my-6 text-orange-300">
+        All Toys: {alltoys.length}
+      </h1>
 
-                         
-
-                        <div className="overflow-x-auto">
-  <table className="table w-full">
-    {/* head*/}
-    <thead>
-      <tr>
-        <th>Seller</th>
-        <th>Toy Name</th>
-        <th>Sub-Category</th>
-        <th>Price</th>
-        <th>Quantity</th>
-        <th>Info</th>
-      </tr>
-    </thead>
-    <tbody>
-      
-    {
-                 alltoys.map(toy => <Toytable
-                 key={toy._id}
-                 toy = {toy}
-                 ></Toytable>)
-         }
-     
-    </tbody>
-  </table>
-</div>
-         
-    
-                </div>
-        );
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          {/* head*/}
+          <thead>
+            <tr>
+              <th>Seller</th>
+              <th>Toy Name</th>
+              <th>Sub-Category</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Info</th>
+            </tr>
+          </thead>
+          <tbody>
+            {alltoys.map((toy) => (
+              <Toytable key={toy._id} toy={toy}></Toytable>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Alltoy;
